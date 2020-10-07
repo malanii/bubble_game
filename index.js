d6ac8aba3273474da29e7ff33f84cfda7499781c
Yana Malanii <miramis134@gmail.com>
2020-10-07 12:26:28 +0300

    try touchend event

--- index.js
+++ index.js
@@ -159,6 +159,20 @@ window.addEventListener('click', () => {
         // console.log(angle)
     }
 );
+
+window.addEventListener('touchend', () => {
+        // console.log(projectiles)
+        const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2);
+        const velocity = {
+            x: Math.cos(angle) * 5,
+            y: Math.sin(angle) * 5,
+        };
+        projectiles.push(new Projectile(canvas.width / 2, canvas.height / 2, 5, "white", velocity))
+        // console.log(angle)
+    }
+);
+
+
 startGameBtn.addEventListener('click', () => {
     init();
     animete();
